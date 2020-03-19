dd0d179aef70448c1e5a7a1ee82b498cb6174dca
here-eeu <39398910+here-eeu@users.noreply.github.com>
2020-03-19 17:51:10 +0300

    HERE Hospital Analytics Tool

---
+++ here-hospital-analytics-tool/precache-manifest.63a3799f74ff7fb6c36bec8a490fbba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7b87ef5ccce36bad1fdb7a71d6b2de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1232c92cf45692fc7255",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c186075689e828b6fff4",
+    "url": "/static/css/main.f09edb83.chunk.css"
+  },
+  {
+    "revision": "1232c92cf45692fc7255",
+    "url": "/static/js/2.c253f838.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.c253f838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c186075689e828b6fff4",
+    "url": "/static/js/main.95e80bd5.chunk.js"
+  },
+  {
+    "revision": "11b4255daec70b59ce87",
+    "url": "/static/js/runtime-main.960cf2bd.js"
+  }
+]);
