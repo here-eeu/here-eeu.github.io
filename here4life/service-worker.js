4a17ad15a308386347118c9c77f410325391e4af
here-eeu <39398910+here-eeu@users.noreply.github.com>
2020-05-18 19:18:03 +0300

    update

--- here4life/service-worker.js
+++ here4life/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/here4life/precache-manifest.563d579e375c4a987f5d47ae7217167b.js"
+  "/here4life/precache-manifest.4d92ae8d114040f408bc0c0a1be7c129.js"
 );
 
 self.addEventListener('message', (event) => {
